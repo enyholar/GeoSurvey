92c46c910a9ff51965a9ad602970e065e9c9c754
GIDDY <gideonoyediran1@gmail.com>
2020-06-08 11:28:22 +0100

    some ui changes

--- app/src/main/java/com/behruz/geosurvey/MainActivity.java
+++ app/src/main/java/com/behruz/geosurvey/MainActivity.java
@@ -3,6 +3,9 @@ package com.behruz.geosurvey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void createLayoutDynamically(int n) {
+
+        for (int i = 0; i < n; i++) {
+            Button myButton = new Button(this);
+            myButton.setText("Button :"+i);
+            myButton.setId(i);
+            final int id_ = myButton.getId();
+
+            LinearLayout layout = (LinearLayout) findViewById(R.id.myDynamicLayout);
+            layout.addView(myButton);
+
+            myButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Toast.makeText(DynamicLayout.this,
+                            "Button clicked index = " + id_, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+        }
 }
